cb42e3a3e399e2711cc46cf34c5f38c5101f8981
Gulshan Kumar <imgulshankrp@gmail.com>
2026-07-26 18:50:29 +0530

    Fix auth API routes

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-resume-analyzer-zf65.onrender.com",
+  baseURL: "https://ai-resume-analyzer-zf65.onrender.com/api/auth",
 });
 
 export const registerUser = (data) => API.post("/register", data);
